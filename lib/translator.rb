85f2ffbbf6dcbe7c62f58d427009bacf4ed5408a
Ziad Zebib <zebibziad@gmail.com>
2021-01-01 22:17:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ def get_japanese_emoticon(path, emoticon_eng)
       return value[:japanese]
     end
   end
+  return  "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emoticon_jpn)
