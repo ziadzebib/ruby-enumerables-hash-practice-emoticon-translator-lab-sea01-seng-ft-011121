66cbde72185aff8d3f24e90b3161507e2aefbc04
Ziad Zebib <zebibziad@gmail.com>
2021-01-01 21:17:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,9 @@ def load_library(path)
 emoticons = YAML.load_file('emoticons.yml')
 new_hash = {}
 emoticons.each do |key, value|
-  new_hash[key] = {:english, :japanese}
-puts emoticons.inspect
+  new_hash[key] = {:english => value[0], :japanese => value[1]}
+end
+puts new_hash
 end
 load_library("")
 def get_japanese_emoticon
