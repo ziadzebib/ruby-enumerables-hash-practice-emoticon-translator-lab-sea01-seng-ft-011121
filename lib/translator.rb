5f4426223cce722a2260958c8947f64da6731906
Ziad Zebib <zebibziad@gmail.com>
2021-01-01 22:03:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,10 +14,10 @@ def get_japanese_emoticon
 end
 
 def get_english_meaning(path, emoticon_jpn)
-english_hash = load_library(path)
-english_hash.each do |key, value|
-  if value[:japanese] == emoticon_jpn
-  return key
+  english_hash = load_library(path)
+  english_hash.each do |key, value|
+    if value[:japanese] == emoticon_jpn
+      return key
+    end
   end
-end
 end
